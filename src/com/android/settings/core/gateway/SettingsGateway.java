PixelBlaster-OS/packages_apps_Settings

Settings: Bring back Gaming mode [2/3]
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Change-Id: Iac1b92118a722c35891feac836667543aa0c3cdc
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -162,6 +162,7 @@ import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 import com.blaster.settings.Explosives;
+import com.blaster.settings.fragments.GamingMode;
 
 public class SettingsGateway {
 
@@ -313,7 +314,8 @@ public class SettingsGateway {
             InteractAcrossProfilesSettings.class.getName(),
             InteractAcrossProfilesDetails.class.getName(),
             MediaControlsSettings.class.getName(),
-            Explosives.class.getName()
+            Explosives.class.getName(),
+            GamingMode.class.getName()            
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -354,5 +356,6 @@ public class SettingsGateway {
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
             Settings.ExplosivesActivity.class.getName(),
+            Settings.GamingModeActivity.class.getName(),            
     };
 }

dotOS-FE/android_packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -162,6 +162,7 @@ import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSetting
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.android.settings.livedisplay.LiveDisplaySettings;
+import com.android.settings.system.GamingMode;
 
 public class SettingsGateway {
 
@@ -312,7 +313,8 @@ public class SettingsGateway {
             InteractAcrossProfilesSettings.class.getName(),
             InteractAcrossProfilesDetails.class.getName(),
             MediaControlsSettings.class.getName(),
-            LiveDisplaySettings.class.getName()
+            LiveDisplaySettings.class.getName(),
+            GamingMode.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -352,5 +354,6 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
+            Settings.GamingModeActivity.class.getName()
     };
 }